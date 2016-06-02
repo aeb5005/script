9c6b20f937700c46bdcb09a4c6ca16dd019af5fb
EC2 Default User <ec2-user@ip-172-31-59-51.ec2.internal>
2016-06-02 02:46:50 +0000

    works, yes command might not 2

--- script.sh
+++ script.sh
@@ -1,45 +1,65 @@
 #!/bin/bash
 
 # nc_out is the traceroute results from the client
-# empty.txt is an empty file
+# intrace_out is the result from intrace from server to client
 
-FILE=nc_out
-PIPE=fifo
+FILE="nc_out"
+FILE2="intrace_out"
+PIPE="fifo"
+PIPE2="fifo2"
 
 if [ ! -e "$FILE" ] ; then
-        touch "$FILE"
+        echo making "$FILE"
+	touch "$FILE"
 fi
 
 if [ ! -w "$FILE" ] ; then
-        echo cannot write to $FILE
+        echo cannot write to "$FILE"
         exit 1
 fi
 
+if [ ! -e "FILE2" ] ; then
+	echo making "$FILE2"
+	touch "$FILE2"
+fi
+
+if [ ! -w "$FILE2" ] ; then
+	echo cannot write to "$FILE2"
+	exit 1
+fi
+
 # making named pipe for nc input
-mkfifo fifo
-exec 3<>fifo
+if [ ! -e "$PIPE" ] ; then 
+	echo making pipe 1
+	mkfifo "$PIPE"
+	exec 3<>"$PIPE"
+fi
 
 # named pipe for intrace input for enter
-mkfifo fifo2
-exec 6<>fifo2
+if [ ! -e "$PIPE2" ] ; then
+	echo making pipe 2
+	mkfifo "$PIPE2"
+	exec 6<>"$PIPE2"
+fi
 
-touch intrace_out
+# is sudo needed? appears so, not sure why 
+# if [ ! pgre (FINISH THIS)
+sudo ./intrace -h 76.99.28.199 -p 44544 0<$PIPE2 1>$FILE2 &
 
-sudo ./intrace -h 76.99.28.199 -p 44544 0<fifo 1>intrace_out &
-
-nc -k -l 44544 0<fifo 1>nc_out &
+nc -k -l 44544 0<$PIPE 1>$FILE &
 
 # if enter, it's go time
 # -q or --quiet exits if match is found with 0 status
-# -e tells echo to interpret escape characters, \n is enter 
+# -e tells echo to interpret escape characters, \n is enter
 # or use yes command
-if [ grep -q "ENTER" intrace_out ] ; then
-	yes '' > fifo2
-fi
-
-# need to start intrace and send to background
-# while diff is 0 (while they are the same do nothing, aka while blank do nothi$
-# else: data is being sent, time to start intrace
-# need to send enter signal to intrace
-# intrace needs to be constantly appending to a file
+done=0
+while [ "$done" -eq 0 ] ; do
+	echo waiting...
+	sleep 1
+	if grep -q "ENTER" intrace_out; then
+		echo InTrace is ready to begin
+		yes '' > "$PIPE2"
+		done=1
+	fi
+done
 
